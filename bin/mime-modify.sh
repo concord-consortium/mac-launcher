6078df09175faaa9bc733670e1ab256fe6578cc0
Aaron Unger <aaron.unger@gmail.com>
2014-01-15 09:16:00 -0800

    Use a fork of appbundler which simplifies things.
    From: https://bitbucket.org/infinitekind/appbundler

--- bin/mime-modify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-plutil -insert CFBundleDocumentTypes -xml '<array><dict><key>CFBundleTypeExtensions</key><array><string>ccla</string></array><key>CFBundleTypeMIMETypes</key><array><string>application/vnd.concordconsortium.launcher</string></array><key>CFBundleTypeName</key><string>CC Launcher Doc</string><key>LSHandlerRank</key><string>Owner</string></dict></array>' dist/app/CCLauncher.app/Contents/Info.plist
